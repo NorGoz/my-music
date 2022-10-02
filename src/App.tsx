cf7aaa26e5fe022aa4bae7c3366536e11f633a82
NorGoz <norbert1303@wp.pl>
2022-10-02 10:27:43 +0200

    add html structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './_app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+        <div className='form__container'>
+            <h2 className='form__title'>Nowy Kontrahent</h2>
+            <form className='form__form'>
+                <label htmlFor="name" className='form__label'><input type="text" id='name' required={true} className='form__input input__text'/><span className='form__text'>Imię</span></label>
+                <label htmlFor="lastName" className='form__label'><input type="text" id='lastName' required={true} className='form__input input__text'/><span className='form__text'>Nazwisko</span></label>
+                <label htmlFor="PIT" className='form__label PIT'>Firma <input type="checkbox" id='PIT' className='form__input'/></label>
+                <label htmlFor="nipOrPit" className='form__label'><input type="text" id='nipOrPit' required={true} className='form__input input__text'/><span className='form__text'>PIT || NIP</span></label>
+                <label htmlFor="upload" className='form__label'>Dodaj Zdjęcie: <input type="file" id='upload' accept='image/png, image/jpeg' className='form__input'/></label>
+                <button className='form__btn btn'>Dodaj</button>
+            </form>
+        </div>
     </div>
   );
 }
